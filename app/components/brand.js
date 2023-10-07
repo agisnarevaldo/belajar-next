84977f91bf6b05dbfa2d0c4e00f273b40a4ab761
Agisna Revaldo <agisrifaldo75@gmail.com>
2023-10-07 08:58:18 +0700

    create a navbar

---
+++ app/components/brand.js
@@ -0,0 +1,8 @@
+export default function Brand() {
+  return (
+    <a className="gap-1.5 w-4 h-11 flex items-center" href="/">
+      <img className="" src="https://help.apple.com/assets/642601949698396B0B0C2097/6426019B9698396B0B0C20AF/id_ID/2f77cc85238452e25cb517130188bf99.png" alt="logo" />
+      Apple
+    </a>
+  )
+}
--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,8 +1,10 @@
 import Link from "next/link"
+import Brand from "./brand"
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between p-10">
+    <nav className="flex justify-between py-5 px-10 max-xl items-center">
+      <Brand />
       <Link href="/">Home</Link>
       <Link href="/about">About</Link>
       <Link href="/blog">Blog</Link>
